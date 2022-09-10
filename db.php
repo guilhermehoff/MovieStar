d79b5603a3c880786bf1e3f4044bb34ac9366a3b
guilhermehoff <guilherme_rossi_hoff@hotmail.com>
2022-09-09 21:14:17 -0300

    atualizarion db

--- db.php
+++ db.php
@@ -3,11 +3,13 @@
   session_start();
 
   $db_name = 'moviestar';
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_pass = '';
+  $db_host = 'us-cdbr-east-06.cleardb.net';
+  $db_user = 'b08a0e92e8dc16';
+  $db_pass = 'f12b2af2';
 
   $conn = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+  mysql://b08a0e92e8dc16:f12b2af2@us-cdbr-east-06.cleardb.net/heroku_b3d79f1e3ee5823?reconnect=true
